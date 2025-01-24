fed5235a72f682fdbd66dc7b6efc8616bafda745
nruharish <bs.harish@gmail.com>
2025-01-24 16:05:44 +0530

    DI in @TEST

--- src/test/java/com/example/demo/Spring6DiApplicationTests.java
+++ src/test/java/com/example/demo/Spring6DiApplicationTests.java
@@ -1,11 +1,31 @@
 package com.example.demo;
 
+import com.example.demo.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
 
+	@Autowired
+	ApplicationContext ctx;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutoWiredCtrl(){
+		System.out.println("In test Autowired");
+		System.out.println(myController.greet());
+	}
+	@Test
+	void testGetControllerFromCtx(){
+		System.out.println("In test");
+		MyController myController = ctx.getBean(MyController.class);
+		System.out.println(myController.greet());
+	}
 	@Test
 	void contextLoads() {
 	}
